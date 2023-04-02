0f1068baa63884683b8850eea676a30b0b2b00dd
RianJacked <phucnguyenhuu34299@gmail.com>
2023-04-02 17:03:03 +0700

    init project

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static int idleState;
+    public static int locomotionState;
+    public static int speedParam;
+    public static int TurnOnSpotLeftState;
+    public static int TurnOnSpotRightState;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+
+        idleState = Animator.StringToHash("Idle");
+        locomotionState = Animator.StringToHash("Locomotion");
+        TurnOnSpotLeftState = Animator.StringToHash("TurnOnSpotLeft");
+        TurnOnSpotRightState = Animator.StringToHash("TurnOnSpotRight");
+        speedParam = Animator.StringToHash("Speed");
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float verticalInput;
+    private float horizontalInput;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        animator.SetFloat(GameController.speedParam, verticalInput);
+
+        // Check if the player is stopped (playing the Idle animation)
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            // If the player is stopped, check horizontal input for turning animations
+            if (horizontalInput < 0)
+            {
+                // Play TurnOnSpotLeftA animation if horizontal input is negative
+                animator.Play(GameController.TurnOnSpotLeftState);
+            }
+            else if (horizontalInput > 0)
+            {
+                // Play TurnOnSpotRightA animation if horizontal input is positive
+                animator.Play(GameController.TurnOnSpotRightState);
+            }
+        }
+    }
+}
+
+
